5bc5ef8909d19d2d232c6854e757d75f5d520470
Ryan Howard <ryanghoward@protonmail.com>
2022-07-18 11:47:20 -0700

    Buliding navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from "react";
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
   return (
     <div>
-      <h1>VHCL</h1>
+      <Navbar />
     </div>
   );
 }
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -4,11 +4,38 @@ import {
   AiOutlineMenu,
   AiOutlineClose,
   AiOutlineSearch,
-  AiOutlineMenu,
+  AiOutlineUser,
 } from "react-icons/ai";
+import Logo from "../../images/DriveLogoDark.png";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <header className={styles.navbar}>
+      <img src={Logo} alt='/' />
+      <nav>
+        <ul className={styles.menu}>
+          <li>
+            <a href='/'>Learn More</a>
+          </li>
+          <li>
+            <a href='/'>Log In</a>
+          </li>
+          <li>
+            <a href='/'>Sign Up</a>
+          </li>
+          <li>
+            <AiOutlineSearch size={25} style={{ marginTop: "6px" }} />
+          </li>
+          <li>
+            <AiOutlineUser size={25} style={{ marginTop: "6px" }} />
+          </li>
+        </ul>
+      </nav>
+      <div className={styles.mobile_btn}>
+        <AiOutlineMenu size={25} />
+      </div>
+    </header>
+  );
 };
 
 export default Navbar;
