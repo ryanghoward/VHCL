fb59b0209975af506ab1d3de5b585cd34febd3ee
Ryan Howard <ryanghoward@protonmail.com>
2022-07-18 11:30:19 -0700

    react-icons installed

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,14 @@
+import React from "react";
+import styles from "./Navbar.module.css";
+import {
+  AiOutlineMenu,
+  AiOutlineClose,
+  AiOutlineSearch,
+  AiOutlineMenu,
+} from "react-icons/ai";
+
+const Navbar = () => {
+  return <div>Navbar</div>;
+};
+
+export default Navbar;
